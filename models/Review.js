9bc0cea819b943ae16566c95b81f308bf8dfd6bc
Hanna Murai <hannamurai@Hannas-MacBook-Pro.local>
2020-11-29 15:14:34 -0600

    mongoose

---
+++ models/Review.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const ReviewSchema = new mongoose.Schema({
+
+})
+
+
+//MONGODB_URI=mongodb+srv://hanna2004:astra2004all@cluster0.kf4hh.mongodb.net/survey?retryWrites=true&w=majority
